8a830e6787892fc59ef84eea26eb8083e4f717d7
Sandip Arsude <Sandip.Arsude@amdocs.com>
2023-04-12 14:59:11 +0530

    add categories to API

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,11 +1,33 @@
-import { productsData } from "../testData.js"
+import { productsData, usersData, categoriesData } from "../testData.js"
 
 const resolvers = {
 	Query: {
 		appName: () =>
 			'ProductHunt clone',
-		allProducts: () => productsData
+		allProducts: () => productsData,
+		productsByAuthor: (_, args) => {
+			const user = usersData.find(user => user.userName === args.authorName)
+			return productsData.filter(product => product.authorId === user.id)
+		},
+		productsByCategory: (_, { slug }) => {
+			const category = categoriesData.find(category => category.slug === slug)
+
+			return productsData.filter(product => product.categoriesIds.includes(category.id))
+		}
 	},
+	Product: {
+		author: (product) => {
+			return usersData.find(user => user.id === product.authorId)
+		},
+		categories: (product) => {
+			const res = product.categoriesIds.map(categoryId => {
+				return categoriesData.find(category => category.id === categoryId)
+			})
+
+			return res
+		}
+	},
+
 }
 
 export default resolvers;
--- server/testData.js
+++ server/testData.js
@@ -5,6 +5,9 @@ export const productsData = [
 		url: 'https://educative.io/',
 		numberOfVotes: 10,
 		publishedAt: '2021-04-05',
+		authorId: '1',
+		// This product belongs to the "Education" category
+		categoriesIds: ['1']
 	},
 	{
 		name: 'Apollo',
@@ -12,6 +15,9 @@ export const productsData = [
 		url: 'https://www.apollographql.com/',
 		numberOfVotes: 5,
 		publishedAt: '2021-01-08',
+		authorId: '2',
+		// This product belongs to the "Frameworks" and "API" categories
+		categoriesIds: ['2', '3']
 	},
 	{
 		name: 'OneGraph',
@@ -19,5 +25,40 @@ export const productsData = [
 		url: 'https://www.onegraph.com',
 		numberOfVotes: 5,
 		publishedAt: '2020-08-22',
+		authorId: '1',
+		// This product belongs to the "API" category
+		categoriesIds: ['3']
+	},
+]
+
+export const usersData = [
+	{
+		id: '1',
+		userName: 'ellen',
+		fullName: 'Ellen James'
+	},
+	{
+		id: '2',
+		userName: 'peter',
+		fullName: 'Peter Miles'
+	},
+]
+
+
+export const categoriesData = [
+	{
+		id: '1',
+		slug: 'education',
+		name: 'Education',
+	},
+	{
+		id: '2',
+		slug: 'frameworks',
+		name: 'Frameworks',
+	},
+	{
+		id: '3',
+		slug: 'api',
+		name: 'API',
 	},
 ]
